63763b0ad9c5b4ce40c542458e0d483da49f0c26
Ricardo Rocha <me@rrocha.uk>
2021-06-05 02:34:07 +0100

    added colors class;

--- src/main/java/Main/Colors.java
+++ src/main/java/Main/Colors.java
@@ -4,6 +4,7 @@ import java.awt.*;
 
 public class Colors {
 
+    //colors list
     public static final Color DEEP_BLUE = new Color(45, 45, 71);
     public static final Color BLOOD_RED = new Color(182, 40, 15);
     public static final Color CUTE_PINK = new Color(236, 170, 247);
